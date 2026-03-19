78c60fca50ef559d571c1512ef2f9c6e3e4ee2b1
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2026-03-19 11:35:16 +0200

    added a new scrpit

--- Assets/MuistiPeli Skriptit/AddButtons.cs
+++ Assets/MuistiPeli Skriptit/AddButtons.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class AddButtons : MonoBehaviour
 {
+    private transform puzzle
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
