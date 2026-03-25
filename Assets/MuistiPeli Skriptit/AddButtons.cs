b1ccaea7d5bef8ff342a8da67c7cc5ebf9d56e6e
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2026-03-25 09:59:07 +0200

    a

--- Assets/MuistiPeli Skriptit/AddButtons.cs
+++ Assets/MuistiPeli Skriptit/AddButtons.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class AddButtons : MonoBehaviour
 {
     [Serializefield]
-    private transform puzzlefield
+    private transform puzzleField
     
     [Serializefield]
     private GameObject button;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = )
+        for(int i = 0; i <8; i++)
+         {
+           GameObject _button = Instantiate(button);
+           _button.name = "" + i;
+           _button.transform.SetParent(puzzleField, false);
+         }
     }
+
 
     // Update is called once per frame
     void Update()
