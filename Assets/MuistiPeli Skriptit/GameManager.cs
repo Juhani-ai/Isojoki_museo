44d425439189d960a55abc847fa45d194ea5f157
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2026-03-26 11:12:39 +0200

    addeda new script

---
+++ Assets/MuistiPeli Skriptit/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<Butt> btns = new List<Button>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetButtons();
+    }
+
+   void GetButtons()
+    {
+        GameObject[] objects = GameObject.findGameObjectsWithTag("puzzle8tn");
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<GetButtons>());
+        }
+    }
+   
+}
