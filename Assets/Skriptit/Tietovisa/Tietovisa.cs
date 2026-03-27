ee5260305123c566027d9efb89c383aa2179533e
henry-9ga <henry.jarvenpaa@vuoksi.fi>
2026-03-27 13:41:14 +0200

    Tietovisan kysymysten toiminnan parannus
    Tietovisa lukee tiedot Assets/Resources/QuizItems kansiosta johon niitä voi lisätä Create new asset -> Quiz -> Item. Valitsee assetin satunnaisesti ja lisää 3 satunnaista väärää vaihtoehtoa, sekoittaen vaihtoehdot painikkeisiin.

--- Assets/Skriptit/Tietovisa/Tietovisa.cs
+++ Assets/Skriptit/Tietovisa/Tietovisa.cs
@@ -3,17 +3,20 @@ using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using UnityEngine.Android;
 
 public class Tietovisa : MonoBehaviour
 {
-    public List<Kysymykset> questions;
+    public List<QuizItemData> items;
+    QuizItemData correctItem;
+    List<QuizItemData> answerItems = new List<QuizItemData>();
 
-    int currentQuestionIndex = 0;
     int lives = 3;
     int score = 0;
     bool hintUsed = false;
 
     public RawImage questionImage;
+    public Button[] answerButtons;
     public TMP_Text[] answerTexts;
 
     public TMP_Text hintText;
@@ -21,30 +24,54 @@ public class Tietovisa : MonoBehaviour
     public GameObject popupPanel;
     public TMP_Text popupText;
 
-    Kysymykset currentQuestion;
-
     private void Start()
     {
-        LoadQuestions();
+        items = new List<QuizItemData>(Resources.LoadAll<QuizItemData>("QuizItems"));
+
+        GenerateQuestion();
     }
 
-    private void LoadQuestions()
+    public void GenerateQuestion()
     {
         hintUsed = false;
 
-        currentQuestion = questions[currentQuestionIndex];
+        correctItem = items[Random.Range(0, items.Count)];
 
-        questionImage.texture = currentQuestion.image;
+        questionImage.texture = correctItem.image;
+
+        answerItems.Clear();
+        answerItems.Add(correctItem);
+
+
+
+        while (answerItems.Count < 4)
+        {
+            QuizItemData randomItem = items[Random.Range(0,items.Count)];
+
+            if (!answerItems.Contains(randomItem))
+            {
+                answerItems.Add(randomItem);
+            }
+        }
+
+        Shuffle(answerItems);
 
         for (int i = 0; i < 4; i++)
         {
-            answerTexts[i].text = currentQuestion.answers[i];
+            int index = i;
+
+            answerTexts[i].text = answerItems[i].itemName;
+
+            answerButtons[i].onClick.RemoveAllListeners();
+            answerButtons[i].onClick.AddListener(() => SelectAnswer(index));
         }
+
+        hintText.text = "";
     }
 
     public void SelectAnswer(int index)
     {
-        if (index == currentQuestion.correctAnswerIndex)
+        if (answerItems[index] == correctItem)
         {
             int points = hintUsed ? 5 : 10;
             score += points;
@@ -56,28 +83,53 @@ public class Tietovisa : MonoBehaviour
             lives--;
             ShowWrongPopup();
         }
+        Debug.Log("Clicked: " + answerItems[index].itemName);
+        Debug.Log("Correct: " + correctItem.itemName);
     }
 
     public void ShowHint()
     {
         hintUsed = true;
-        hintText.text = currentQuestion.hint;
+        hintText.text = correctItem.hint;
     }
 
     public void ShowCorrectPopup()
     {
-        popupText.text = "Oikein!\n\n" + currentQuestion.infoText;
+        popupText.text = "Oikein!\n\n" + correctItem.info;
         popupPanel.SetActive(true);
     }
 
     public void ShowWrongPopup()
     {
-        string correct = currentQuestion.answers[currentQuestion.correctAnswerIndex];
+        string correct = correctItem.itemName;
 
         popupText.text = "V‰‰rin!\nOikea vastaus on: " + correct +
-            "\n\n" + currentQuestion.infoText;
+            "\n\n" + correctItem.info;
 
         popupPanel.SetActive(true);
+    }
+
+    public void NextQuestion()
+    {
+        if (lives <= 0)
+        {
+            // GameOver();
+            return;
+        }
+
+        GenerateQuestion();
+    }
+
+    void Shuffle(List<QuizItemData> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int rand = Random.Range(i, list.Count);
+
+            QuizItemData temp = list[i];
+            list[i] = list[rand];
+            list[rand] = temp;
+        }
     }
 }
  
