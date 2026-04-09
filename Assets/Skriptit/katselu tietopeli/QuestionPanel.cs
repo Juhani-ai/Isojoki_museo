e1da745257ea8b1e71d0e4e370436681c69b2364
NikiKallela <niki.kallela@vuoksi.fi>
2026-04-09 15:19:48 +0300

    muokattu koodi

--- Assets/Skriptit/katselu tietopeli/QuestionPanel.cs
+++ Assets/Skriptit/katselu tietopeli/QuestionPanel.cs
@@ -12,38 +12,82 @@ public class QuestionPanel : MonoBehaviour
     [SerializeField] private Button wrongButton3;
 
     [SerializeField] private Button nextButton;
+    [SerializeField] private TMP_Text nextText;
     [SerializeField] private TMP_Text rightText;
+    [SerializeField] private TMP_Text wrongText;
+    [SerializeField] private Button answerButton;
+    [SerializeField] private TMP_Text answerText;
+    [SerializeField] private RawImage answerImage;
+    [SerializeField] private TMP_Text answerbuttonText;
 
     void Start()
     {
-    
-        nextButton.gameObject.SetActive(false);
-        rightText.gameObject.SetActive(false);
+ 
+        nextButton.interactable = false;
 
         correctButton.onClick.AddListener(CorrectClicked);
         wrongButton1.onClick.AddListener(() => WrongClicked(wrongButton1));
         wrongButton2.onClick.AddListener(() => WrongClicked(wrongButton2));
         wrongButton3.onClick.AddListener(() => WrongClicked(wrongButton3));
+        nextButton.onClick.AddListener(() => Debug.Log("Toimii"));
+        
+        answerButton.onClick.AddListener(AnswerClicked);
        
     }
 
     void WrongClicked(Button btn)
     {
-        btn.image.color = Color.red;
-        btn.interactable = false;
-    }
-
-    void CorrectClicked()
-    {
+        nextButton.interactable = true;
+    
         correctButton.gameObject.SetActive(false);
         wrongButton1.gameObject.SetActive(false);
         wrongButton2.gameObject.SetActive(false);
         wrongButton3.gameObject.SetActive(false);
 
-        nextButton.gameObject.SetActive(true);
+        nextButton.image.color = Color.white;
+        nextButton.onClick.AddListener(() => Debug.Log("Toimii"));
+
+        wrongText.gameObject.SetActive(true);
+        answerButton.gameObject.SetActive(true);
+        answerText.gameObject.SetActive(true);
+        answerImage.gameObject.SetActive(true);
+    }
+
+    void CorrectClicked()
+    {
+        rightText.gameObject.SetActive(true);
+
+        nextButton.interactable = true;
+        correctButton.gameObject.SetActive(false);
+        wrongButton1.gameObject.SetActive(false);
+        wrongButton2.gameObject.SetActive(false);
+        wrongButton3.gameObject.SetActive(false);
+
+        nextText.color = Color.black;
+        nextButton.image.color = Color.white;
         rightText.gameObject.SetActive(true);
 
         nextButton.onClick.RemoveAllListeners();
-        nextButton.onClick.AddListener(() => Debug.Log("Seuraava vaihe!"));
+        nextButton.onClick.AddListener(() => Debug.Log("Toimii "));
+    }
+
+    void AnswerClicked()
+    {
+        if (answerbuttonText.text == "Paljasta oikea vastaus")
+        {
+            answerbuttonText.text = "Piilota oikea vastaus";
+            answerButton.image.color = Color.red;
+
+            answerImage.gameObject.SetActive(false);
+        }
+
+        else if (answerbuttonText.text == "Piilota oikea vastaus")
+        {
+            answerbuttonText.text = "Paljasta oikea vastaus";
+            answerButton.image.color = Color.green;
+
+            answerImage.gameObject.SetActive(true);
+        }
+       
     }
 }
