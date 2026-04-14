097d8308df350efc92301dc49b535df30ab28d4b
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2026-04-14 14:05:50 +0300

    game now restarts when it hits zero

--- Assets/MuistiPeli Skriptit/TimeScript.cs
+++ Assets/MuistiPeli Skriptit/TimeScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
  
 
@@ -7,21 +8,64 @@ public class TimeScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    [SerializeField] TextMeshProUGUI timerText;
    [SerializeField] float remainingTime;
+
+    [Header("Game Over")]
+    [SerializeField] private bool restartOnGameOver = true;
+    [SerializeField] private float restartDelaySeconds = 2f;
+    [SerializeField] private string gameOverMessage = "Peli ohi";
+
+    private bool gameOverTriggered;
     
         
     // Update is called once per frame
     void Update()
     {
+        if (gameOverTriggered) return;
+
         remainingTime -= Time.deltaTime;
 
         if (remainingTime <= 0)
         {
             remainingTime = 0;
-            // GameOver();
-            timerText.color = Color.red;
+            GameOver();
+            return;
         }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (timerText != null)
+        {
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOverTriggered = true;
+
+        if (timerText != null)
+        {
+            timerText.color = Color.red;
+            timerText.text = gameOverMessage;
+        }
+
+        if (restartOnGameOver)
+        {
+            StartCoroutine(RestartAfterDelay());
+        }
+    }
+
+    private System.Collections.IEnumerator RestartAfterDelay()
+    {
+        float delay = Mathf.Max(0f, restartDelaySeconds);
+        if (delay > 0f)
+        {
+            // Use realtime so restart still happens if Time.timeScale is changed elsewhere.
+            yield return new WaitForSecondsRealtime(delay);
+        }
+
+        // Reload current scene (simple restart)
+        var scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
