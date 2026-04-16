bc20be7d1368b83aa5605f76cd997ba7dd41e67e
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2026-04-16 10:40:59 +0300

    added a card with names of the objects

--- Assets/MuistiPeli Skriptit/PoistuScript.cs
+++ Assets/MuistiPeli Skriptit/PoistuScript.cs
@@ -1,15 +1,44 @@
+/*
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PoistuScript : MonoBehaviour
 {
-	public void QuitGame()
-	{
-		// In a built player, this closes the application.
-		Application.Quit();
+    [Header("Where to go when exiting the minigame")]
+    [SerializeField] private string exitSceneName = "YourNewSceneName";
 
-		// In the Unity Editor, Application.Quit() does nothing, so stop Play Mode.
+    [Header("Things to stop (assign in Inspector)")]
+    [SerializeField] private TimeScript timeScript;
+    [SerializeField] private MuistipeliScoreScript scoreScript;
+    [SerializeField] private GameManager gameManager;
+
+    public void ExitToScene()
+    {
+        // Stop timer + prevent game over restart logic from firing
+        if (timeScript != null)
+        {
+            timeScript.StopTimer();
+            timeScript.StopAllCoroutines();
+            timeScript.enabled = false;
+        }
+
+        // Stop scoring updates
+        if (scoreScript != null)
+            scoreScript.enabled = false;
+
+        // Optional: stop input/game logic immediately
+        if (gameManager != null)
+            gameManager.enabled = false;
+
+        SceneManager.LoadScene(exitSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
 #if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false;
 #endif
-	}
+    }
 }
+*/
