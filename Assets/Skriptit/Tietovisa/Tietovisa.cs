e73cade577762731c70b31f0dc38d3b47e5664c9
henry-9ga <henry.jarvenpaa@vuoksi.fi>
2026-05-06 14:40:48 +0300

    Tietovisa toimii
    Tietovisan pitäisi nyt olla täysin toimiva. Tekstejä pitää vielä lisäillä QuizItemeihin vihjeille ja infolle. Valitsee kysymyksen satunnaisesti. Peli loppuu kymmenen kysymyksen jälkeen, tai jos elämät loppuvat. Tallentaa pisteet koko pelin laajuisesti.

--- Assets/Skriptit/Tietovisa/Tietovisa.cs
+++ Assets/Skriptit/Tietovisa/Tietovisa.cs
@@ -10,24 +10,42 @@ public class Tietovisa : MonoBehaviour
     public List<QuizItemData> items;
     QuizItemData correctItem;
     List<QuizItemData> answerItems = new List<QuizItemData>();
+    List<QuizItemData> remainingItems = new List<QuizItemData>();
 
     int lives = 3;
     int score = 0;
     bool hintUsed = false;
+    public TMP_Text scoreText;
+    public TMP_Text livesText;
 
     public RawImage questionImage;
     public Button[] answerButtons;
     public TMP_Text[] answerTexts;
 
+    public GameObject nextButton;
+    public GameObject replayButton;
+    public GameObject menuButton;
+
+    public GameObject hintButton;
     public TMP_Text hintText;
 
     public GameObject popupPanel;
     public TMP_Text popupText;
 
+    int maxQuestions = 10;
+    int questionsAsked = 0;
+
     private void Start()
     {
         items = new List<QuizItemData>(Resources.LoadAll<QuizItemData>("QuizItems"));
 
+        remainingItems = new List<QuizItemData>(items);
+
+        UpdateUI();
+    }
+
+    public void StartGame()
+    {
         GenerateQuestion();
     }
 
@@ -35,7 +53,10 @@ public class Tietovisa : MonoBehaviour
     {
         hintUsed = false;
 
-        correctItem = items[Random.Range(0, items.Count)];
+        int randIndex = Random.Range(0, remainingItems.Count);
+        correctItem = remainingItems[randIndex];
+
+        remainingItems.RemoveAt(randIndex);
 
         questionImage.texture = correctItem.image;
 
@@ -67,22 +88,29 @@ public class Tietovisa : MonoBehaviour
         }
 
         hintText.text = "";
+        hintButton.SetActive(true);
     }
 
     public void SelectAnswer(int index)
     {
         if (answerItems[index] == correctItem)
         {
-            int points = hintUsed ? 5 : 10;
+            int points = hintUsed ? 10 : 15;
             score += points;
+
+            Pistemanageri.LisaaPisteita(score);
 
             ShowCorrectPopup();
         }
         else
         {
             lives--;
+            lives = Mathf.Max(0, lives);
             ShowWrongPopup();
         }
+
+        UpdateUI(); 
+
         Debug.Log("Clicked: " + answerItems[index].itemName);
         Debug.Log("Correct: " + correctItem.itemName);
     }
@@ -91,6 +119,7 @@ public class Tietovisa : MonoBehaviour
     {
         hintUsed = true;
         hintText.text = correctItem.hint;
+        hintButton.SetActive(false);
     }
 
     public void ShowCorrectPopup()
@@ -111,12 +140,29 @@ public class Tietovisa : MonoBehaviour
 
     public void NextQuestion()
     {
+        popupPanel.SetActive(false);
+
         if (lives <= 0)
         {
-            // GameOver();
+            Lose();
+            Debug.Log("Lose");
             return;
         }
 
+        if (questionsAsked >= maxQuestions)
+        {
+            Win();
+            Debug.Log("Win (reached question limit");
+            return;
+        }
+
+        if (remainingItems.Count == 0)
+        {
+            Win();
+            Debug.Log("Win (no more questions");
+            return;
+        }
+        questionsAsked++;
         GenerateQuestion();
     }
 
@@ -130,6 +176,59 @@ public class Tietovisa : MonoBehaviour
             list[i] = list[rand];
             list[rand] = temp;
         }
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = $"Pisteet: {score}";
+        livesText.text = $"Elamat: {lives}";
+    }
+
+    public void Lose()
+    {
+        popupText.text = $"Voi harmi, elamat loppui.\n\nPisteet: {score}";
+        popupPanel.SetActive(true);
+        nextButton.SetActive(false);
+        replayButton.SetActive(true);
+        menuButton.SetActive(true);
+    }
+
+    public void Win()
+    {
+        popupText.text = $"Onneksi olkoon!\n\nPisteet: {score}";
+        popupPanel.SetActive(true);
+        nextButton.SetActive(false);
+        replayButton.SetActive(true);
+        menuButton.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        Pistemanageri.TallennaPisteet();
+        ResetGame();
+        GenerateQuestion();
+    }
+    public void ReturnToMenu()
+    {
+        Pistemanageri.TallennaPisteet();
+        ResetGame();
+    }
+
+    void ResetGame()
+    {
+        score = 0;
+        lives = 3;
+        questionsAsked = 0;
+
+        items = new List<QuizItemData>(Resources.LoadAll<QuizItemData>("QuizItems"));
+        remainingItems = new List<QuizItemData>(items);
+
+        UpdateUI();
+
+        nextButton.SetActive(true);
+        replayButton.SetActive(false);
+        menuButton.SetActive(false);
+        popupPanel.SetActive(false);
     }
 }
  
